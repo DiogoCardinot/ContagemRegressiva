16272592b1ce2525b3d343ea40c85904dbb0f966
Diogo Cardinot <diogocardinotuerj@gmail.com>
2022-01-19 15:22:12 -0300

    Continua contagem depois de recarregar a pagina

--- main.js
+++ main.js
@@ -1,71 +1,75 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const botao = document.querySelector("#submit-button");
+  const startButton = document.querySelector("#start-button");
 
-  botao.addEventListener("click", imprimeData);
-  botao.addEventListener("reload", imprimeData);
+  startButton.addEventListener("click", calcDate);
+  function calcDate() {
+    let updateInterval = setInterval(function () {
+      let inputEndDate = document.querySelector("#date-input"); //input
+      let inputEndDateValue = inputEndDate.value; //valor do input
+      let endDate = new Date(inputEndDateValue).getTime(); //tranforma o valor do input em uma data
+      let now = new Date().getTime(); //data de 'hoje'
 
-  function imprimeData() {
-    var x = setInterval(function () {
-      var countDownDate = document.querySelector("#date-input"); //data do input
-      var count = countDownDate.value;
-      var endDate = new Date(count).getTime();
-      var now = new Date().getTime(); //data de 'hoje'
+      let distance = endDate - now; //tempo que falta para a data de hoje até a data do input em ms;
+      if (distance <= 0 || inputEndDateValue === "") {
+        clearInterval(updateInterval);
 
-      var distance = endDate - now; //tempo que falta para a data de hoje até a data do input em ms;
-      if (distance < 0 || count === "") {
-        clearInterval(x);
-        document.getElementById(
-          "time-left-desktop"
+        document.querySelector(
+          "#time-left-desktop"
         ).innerHTML = ` <p class='content-time-left-number'>00:</p>
                           <p class='content-time-left-number'>00:</p>
                           <p class='content-time-left-number'>00:</p>
                           <p class='content-time-left-number'>00</p>`;
 
-        document.getElementById(
-          "time-left-mobile"
+        document.querySelector(
+          "#time-left-mobile"
         ).innerHTML = ` <p class='content-time-left-number'>00:</p>
                           <p class='content-time-left-number'>00:</p>
                           <p class='content-time-left-number'>00min:</p>
                           <p class='content-time-left-number'>00sec</p>`;
       } else {
-        var oneDay = 60 * 60 * 24 * 1000; //60s * 60min * 24hs * 1000ms
-        var oneHour = 60 * 60 * 1000; //60s*60min*1000ms
-        var oneMinute = 1000 * 60; //1000ms * 60s
-        var oneSecond = 1000; //1000ms
+        let oneDay = 60 * 60 * 24 * 1000; //60s * 60min * 24hs * 1000ms
+        let oneHour = 60 * 60 * 1000; //60s*60min*1000ms
+        let oneMinute = 1000 * 60; //1000ms * 60s
+        let oneSecond = 1000; //1000ms
 
-        var days = Math.floor(distance / oneDay);
-        var hours = Math.floor(
+        let days = Math.floor(distance / oneDay);
+        let hours = Math.floor(
           (distance % oneDay) / oneHour //o resto do dia em segundos / 1000ms*60s*60m = 1hr
         );
-        var minutes = Math.floor((distance % oneHour) / oneMinute); // o resto
-        var seconds = Math.floor((distance % oneMinute) / oneSecond);
+        let minutes = Math.floor((distance % oneHour) / oneMinute); // o resto
+        let seconds = Math.floor((distance % oneMinute) / oneSecond);
 
         // Display the result in the element with id="demo"
-        document.getElementById(
-          "time-left-desktop"
+        document.querySelector(
+          "#time-left-desktop"
         ).innerHTML = ` <p class='content-time-left-number' id='remaining-days'>${days}:</p>
                           <p class='content-time-left-number' id='remaining-hours'>${hours}:</p>
                           <p class='content-time-left-number' id='remaining-minutes'>${minutes}:</p>
                           <p class='content-time-left-number' id='remaining-seconds'>${seconds}</p>`;
 
-        document.getElementById(
-          "time-left-mobile"
+        document.querySelector(
+          "#time-left-mobile"
         ).innerHTML = ` <p class='content-time-left-number' id='remaining-days'>${days} d:</p>
                           <p class='content-time-left-number' id='remaining-hours'>${hours} h:</p>
                           <p class='content-time-left-number' id='remaining-minutes'>${minutes}min:</p>
                           <p class='content-time-left-number' id='remaining-seconds'>${seconds}sec</p>`;
+
+        inputEndDate.addEventListener("change", () => {
+          clearInterval(updateInterval);
+          alert("A");
+        }); //caso o usuário mude o input ele terá que iniciar novamente a contagem
       }
 
-      localStorage.setItem("input", count);
+      localStorage.setItem("input", inputEndDateValue);
       localStorage.getItem("input");
-
-      reloadInput();
     }, 1000);
   }
+
   onload = function reloadInput() {
     var inputValue = localStorage.getItem("input");
+    var countEndDate = document.querySelector("#date-input");
 
-    var count = document.querySelector("#date-input");
-    count.value = inputValue;
+    countEndDate.value = inputValue;
+    calcDate();
   };
 });
